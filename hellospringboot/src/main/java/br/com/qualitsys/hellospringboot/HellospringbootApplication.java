6191a6ac70766a560d99c0557ca92ef7084fe396
Aparecido Freitas <aparecidovfreitas@gmail.com>
2020-07-19 18:52:57 -0300

    v1.0.0 - 19/07/2020

---
+++ hellospringboot/src/main/java/br/com/qualitsys/hellospringboot/HellospringbootApplication.java
@@ -0,0 +1,17 @@
+package br.com.qualitsys.hellospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HellospringbootApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringbootApplication.class, args);
+	}
+
+}
+
+
+ 
